6a75f9abb0db6d88437be0b72cb25652cb01b21d
Pravin Terdal <your-github-email@example.com>
2026-09-21 03:23:29 +0530

    Update React project

---
+++ src/component/register/register.jsx
@@ -0,0 +1,12 @@
+
+
+
+export function Register(){
+
+
+    return(
+        <div className="container-fluid mt-5">
+            <h2 className="text-center">Register component</h2>
+        </div>
+    )
+}
